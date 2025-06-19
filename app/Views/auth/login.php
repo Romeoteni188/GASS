8dd3a729f9df58d0f0f2b11814b8b1bd0da36334
Romeoteni188 <romeoteni093@gmail.com>
2025-06-19 14:29:20 -0600

    feat: implement tailwind

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,8 +1,10 @@
 <?php include '../layouts/header.php'; ?>
+<div class="bg-amber-400 min-h-screen flex items-center justify-center">
 <form method="POST" action="/login">
     <input type="text" name="email" placeholder="Correo">
     <input type="password" name="password" placeholder="Contraseña">
     <button type="submit">Iniciar sesión</button>
 </form>
+</div>
 <?php include '../layouts/footer.php'; ?>
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./app/Views/**/*.php"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
